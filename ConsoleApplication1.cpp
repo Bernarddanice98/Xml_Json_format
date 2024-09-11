623abb94030b3c107a5865ad0a47b70645c66ad4
Класс <class@vyatsu.ru>
2024-09-11 14:47:44 +0300

    Create class without constructor, with set method, get method

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,61 @@
+﻿#include <iostream>
+
+class AirConditioner {
+private:
+    int temperature;
+    int blowing_intensity;
+public:
+    int getTemp() {
+        return temperature;
+    }
+
+    int getBlowing() {
+        return blowing_intensity;
+    }
+
+    void setTemp(int a) {
+        temperature = a;
+    }
+    void setBlowing(int b) {
+        blowing_intensity = b;
+    }
+
+    void t_minus() {
+        
+        if (temperature < 30 && temperature>10) {
+            temperature--;
+        }
+       
+    }
+    void t_plus(int ) {
+        if (temperature >10 && temperature<30) {
+            temperature++;
+        }
+
+    }
+
+    void b_minus() {
+        if (blowing_intensity < 10 && blowing_intensity>1) {
+            blowing_intensity--;
+        }
+        else if (blowing_intensity <1) {
+            blowing_intensity = 10;
+        }
+    }
+    void b_plus() {
+        if (blowing_intensity < 10 && blowing_intensity>1) {
+            blowing_intensity++;
+        }
+        else if (blowing_intensity > 10) {
+            blowing_intensity = 1;
+        }
+    }
+};
+
+
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
